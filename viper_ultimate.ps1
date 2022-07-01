120f79ecc8f75b5fe18b9e126d6fbdf58895c6cc
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2022-07-01 16:50:21 +0800

    fix: automatically get pythonw path

--- viper_ultimate.ps1
+++ viper_ultimate.ps1
@@ -6,8 +6,9 @@ $time_interval = ($default_time_interval, $time_interval)[[bool]$time_interval]
 # install python packages
 python.exe -m pip install -r ./requirements.txt
 
-$p = (Split-Path -Parent $MyInvocation.MyCommand.Definition)  + '\viper_ultimate.pyw'
+$pythonw_path =  (Get-Command -Name pythonw).Source
+$script_path = (Split-Path -Parent $MyInvocation.MyCommand.Definition)  + '\viper_ultimate.pyw'
 # set scheduled task, run script every 15 minutes
-$action = New-ScheduledTaskAction -Execute 'pythonw.exe' -Argument $p
+$action = New-ScheduledTaskAction -Execute $pythonw_path -Argument $script_path
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes $time_interval)
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Viper Ultimate Battery Indicator" -Description "A Python script that shows the battery level of a Razer Viper Ultimate mouse as a tray notification every 60 minutes by default."
